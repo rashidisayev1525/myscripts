a7848f15846ddfd50fc2fcac30777928ad95fd5a
rashidisayev <rashidisayev@gmail.com>
2020-07-29 20:25:34 +0200

    Trigger ready

--- check_cert.sh
+++ check_cert.sh
@@ -4,6 +4,6 @@ if openssl x509 -checkend 86400 -noout -in /etc/letsencrypt/live/vm.rashidisayev
 then
   echo "Certificate is good for another day!"
 else
-  "
+ echo "Is expired or so within 24 hour"
 
 fi
