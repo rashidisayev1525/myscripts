a6082f44dbbbccbd593feeb921c8fd678917ca8c
rashidisayev <rashidisayev@gmail.com>
2020-07-29 11:52:51 +0200

    Trigger ready and telegram notification added

--- check_cert.sh
+++ check_cert.sh
@@ -4,5 +4,6 @@ if openssl x509 -checkend 86400 -noout -in /etc/letsencrypt/live/vm.rashidisayev
 then
   echo "Certificate is good for another day!"
 else
-  echo "Certificate of vm.rashidisayev.com has expired or will do so within 24 hours!"
+  curl -X POST "https://api.telegram.org/bot903089995:AAE2uS2_eVNyl3-oUeHovjI1Kan5-7hF5qo/sendMessage" -d "chat_id=-1001157819373&text=Certificate of vm.rashidisayev.com has expired or will do so within 24 hours! Please inform Rashid Isayev or Mohsen Dalil"
+
 fi
