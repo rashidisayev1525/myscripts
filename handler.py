416ee8bdbf622d4ddfb53540a9dd864cec877fa0
rashidisayev <rashidisayev@gmail.com>
2020-08-24 17:39:33 +0200

    bot integration

--- handler.py
+++ handler.py
@@ -1,46 +1,120 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+import sys
 import requests
-
-def telegram_bot_sendtext(bot_message):
-    bot_token = '1250501014:AAHuPe2lgjcTIFm9Z53V5ver5X2oRuImH8g'
-    bot_chatID = '589954195'
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-
-    response = requests.get(send_text)
-
-    return response.json()
-
-session = requests.session()
-url = 'https://service.berlin.de/dienstleistung/324873/standort/123613/'
-data = urlopen(url)
-
-soup = BeautifulSoup(data, 'html.parser')
-products = soup.findAll('div', {'class': 'content-marginal-termin'})
-for p in products:
-    link = p.find('a')['href']
-
-r = session.get(link)
-parseData = BeautifulSoup(r.content,'html.parser')
-findDates = parseData.findAll('td',{'class':'buchbar'})
-findmonth = parseData.findAll('th',{'class':'month'})
-
-for m in findmonth:
-    month = m.text.strip()
-
-for t in findDates:
-    time = t.find('a')['href']
-    date = t.text.strip()
-    bot_msg = 'Available date for:\n Kita-Gutschein am Standort Jugendamt Treptow-Köpenick - Kitagutscheinverfahren und Hort\n'+date+month+'\nfor booking:\n'+url
-    telegram_bot_sendtext(bot_msg)
-   # print(date+month)
+import telebot
+from bs4 import BeautifulSoup
+from telebot import types
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintoshf  Mac OS X 10_12_6) AppeWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36'
+}
+bot = telebot.TeleBot('')
+chat_id = ''
+keyboard1 = types.ReplyKeyboardMarkup(True, True)
+keyboard1.row('Anmeldung', 'KitaGutschein')
 
 
-base_url = 'https://service.berlin.de'
-completeUrl = base_url+time
-chooseTime = session.get(completeUrl)
-parseTime = BeautifulSoup(chooseTime.content,'html.parser')
-showTime = parseTime.findAll('h1',{'class': 'title'})
-session.close()
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Welcome to terminator!Please choose service in Burgeramt:)',
+                     reply_markup=keyboard1)
 
 
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'anmeldung':
+        bot.send_message(message.chat.id, 'Wait please searching...')
+        session = requests.session()
+        url = 'https://service.berlin.de/dienstleistung/120686/'
+
+        data = requests.get(url, headers)
+        soup = BeautifulSoup(data.text, 'html.parser')
+        products = soup.findAll('div', {'class': 'zmstermin-multi inner'})
+        for p in products:
+            link = p.find('a')['href']
+            r = session.get(link)
+            parseData = BeautifulSoup(r.text, 'html.parser')
+            findDates = parseData.findAll('td', {'class': 'buchbar'})
+            findmonth = parseData.findAll('th', {'class': 'month'})
+        if len(findDates) == 0:
+            bot.send_message(chat_id, "No available dates or smth. wrong with website!")
+            sys.exit()
+
+        for m in findmonth:
+            month = m.text.strip()
+
+        for t in findDates:
+            timeLink = t.find('a')['href']
+            getTime = 'https://service.berlin.de' + timeLink
+            params = {
+                'termin': 1,  # Not sure if necessary
+                'dienstleisterlist': ',', '123613'
+                                          'anliegen[]': '324873',
+        }
+            timeList = requests.get(getTime, params=params, headers=headers)
+            semiFinalTime = BeautifulSoup(timeList.text, 'html.parser')
+            finalTime = semiFinalTime.findAll('th', {'class': 'buchbar'})
+            date = t.text.strip()
+            msgToBeSend = date+month+url+finalTime
+            bot.send_message(chat_id,msgToBeSend)
+
+    elif message.text.lower() == 'kitagutschein':
+        bot.send_message(message.chat.id, 'Wait please searching...')
+    else:
+        bot.send_message(message.chat.id, "Alinmadi")
+
+
+bot.polling()
+
+# ALL_BUERGERAMTS = (
+#     122210, 122217, 327316, 122219, 327312, 122227, 122231, 327346, 122243, 327348, 122252, 329742, 122260, 329745,
+#     122262, 329748, 122254, 329751, 122271, 327278, 122273, 122277, 327276, 122280, 327294, 122282, 122284, 122291,
+#     327270, 122285, 327266, 122286, 327264, 122296, 327268, 150230, 122301, 122297, 122294, 122312, 329763, 122304,
+#     327330, 122311, 327334, 122309, 327332, 122281, 122279, 122276, 122274, 122267, 122246, 327318, 122251, 327320,
+#     122257, 122208, 122226
+# )
+
+# session = requests.session()
+#
+# # url = 'https://service.berlin.de/dienstleistung/324873/standort/123613/'
+# # data = urlopen(url)
+# url = 'https://service.berlin.de/dienstleistung/120686/'
+# data = requests.get(url, headers)
+# soup = BeautifulSoup(data.text, 'html.parser')
+# products = soup.findAll('div', {'class': 'zmstermin-multi inner'})
+#
+# for p in products:
+#     link = p.find('a')['href']
+#
+#     r = session.get(link)
+#
+#
+# parseData = BeautifulSoup(r.text, 'html.parser')
+# findDates = parseData.findAll('td', {'class': 'buchbar'})
+# findmonth = parseData.findAll('th', {'class': 'month'})
+#
+#
+#
+# # if len(findDates) == 0:
+# #     # telegram_bot_sendtext('No available dates')
+# #     bot.send_message(chat_id, "No available dates!")
+# #     sys.exit()
+# #
+# for m in findmonth:
+#      month = m.text.strip()
+# print(month)
+# #
+# for t in findDates:
+#      dates = t.text.strip()
+#      print(dates)
+
+
+    # print(timeLink)
+#     getTime = 'https://service.berlin.de' + timeLink
+#     params = {
+#         'termin': 1,  # Not sure if necessary
+#         'dienstleisterlist': ',', '123613'
+#                                  'anliegen[]': '324873',
+#    }
+#     timeList = requests.get(getTime, params=params, headers=headers)
+#     semiFinalTime = BeautifulSoup(timeList.text, 'html.parser')
+#     finalTime = semiFinalTime.findAll('th', {'class': 'buchbar'})
+#     date = t.text.stip()
